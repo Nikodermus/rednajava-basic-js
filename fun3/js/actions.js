461c7dca0e006cefffb83a65ea5a6e7bb3db544a
Nicolas M. Pardo <nicolasm@nikodermus.media>
2017-03-25 02:04:58 -0500

    fun examples, explore them!

---
+++ fun3/js/actions.js
@@ -0,0 +1,9 @@
+comcastifyjs.letsPrepareTheseImages();
+
+
+window.onload = comcastifyjs.fixMyImagesLoadingSoFast({
+    boxColor: '#ffffff',
+    loadMaxPercent: 1,
+    loadSpeed: 500,
+    progressiveJPEG: true
+});
