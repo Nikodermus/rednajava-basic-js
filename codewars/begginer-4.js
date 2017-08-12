c643be30fd1b07d27b8e914fd6f559a3245df2dc
Nicolas M. Pardo <nicolasm@nikodermus.media>
2017-08-11 22:28:31 -0500

    Removed Codewars
    And created one single repository for katas only
    
    https://github.com/Nikodermus/codewars

--- codewars/begginer-4.js
+++
@@ -1,4 +0,0 @@
-function cockroachSpeed(s) {
-    return Math.floor((s * 100) / 3.6);
-
-}
--- codewars/isogram.js
+++
@@ -1,9 +0,0 @@
-function isIsogram(str) {
-    str = str.toLowerCase()
-    str = str.split('');
-    var status = str.some(function (value, index) {
-        return str.indexOf(value, index + 1) > -1;
-    });
-    return !status;
-
-}
